f71de40f34d7c9a27d0423b11318b25f63ad020f
Netaly Ramirez <21netaly@gmail.com>
2020-03-18 02:53:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // add solution here
+
+function theBeatlesPlay(arrMu,arrIns){
+  let newArr = []
+  for (let i = 0; i <arrMu.length; i ++){
+    let currentMusician = arrMu[i]
+    let currentInstru = arrIns[i]
+    newArr.push(`${currentMusician} plays ${currentInstru}`)
+  }return newArr
+}
+
+function johnLennonFacts(arr){
+  let i = 0
+    i +=1
+  let copy = []
+  while (i <= arr.length){
+    copy.push(arr.slice(arr[i], arr[i].length-1) + "!!!")
+  }return copy
+}
